864d14dba48d8d63d388c04398373b37758b4019
assc0d3r <latrenda32@debutqx.com>
2022-08-06 11:15:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,28 +711,50 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"<b>Hey {message.from_user.mention}, Here is What I Found In My Database For Your Query {search}.</b>"
     if imdb and imdb.get('poster'):
         try:
+
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)
-            await hehe.delete()
+
+            await asyncio.sleep(10)
+
             await message.delete()
+
+            await hehe.delete()            
+
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+
             pic = imdb.get('poster')
+
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
+
             hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)
-            await hmm.delete()
+
+            await asyncio.sleep(10)
+
             await message.delete()
+
+            await hmm.delete()          
+
         except Exception as e:
+
             logger.exception(e)
-            fek = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)
-            await fek.delete()
+
+            fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(10)
+
             await message.delete()
+
+            await fek.delete()           
+
     else:
-        fuk = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(300)
-        await fuk.delete()
+
+        fuk = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+
+        await asyncio.sleep(10)
+
         await message.delete()
+
+        await fuk.delete()
 
 
 async def advantage_spell_chok(msg):
